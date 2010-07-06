2010453bcaaaec2d5cfc59ddd39af0e383d306a2
shaunsephton <shaun@praekelt.com>
2010-07-06 14:16:41 +0200

    pass through status

--- likes/templatetags/likes_inclusion_tags.py
+++ likes/templatetags/likes_inclusion_tags.py
@@ -7,9 +7,11 @@ register = template.Library()
 @register.inclusion_tag('likes/inclusion_tags/likes.html', takes_context=True)
 def likes(context, obj):
     request = context['request']
+    can_vote, vote_status = obj.can_vote(request)
     context.update({
         'content_obj': obj,
-        'can_vote': obj.can_vote(request),
+        'can_vote': can_vote,
+        'vote_status': vote_status,
         'content_type': "-".join((obj._meta.app_label, obj._meta.module_name))
     })
     return context
