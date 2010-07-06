6c004c40d91620ea89f8015298d1f5e6c2e1db47
shaunsephton <shaunsephton@gmail.com>
2010-07-06 15:06:07 +0200

    added vote_status

--- likes/views.py
+++ likes/views.py
@@ -7,7 +7,7 @@ def like(request, content_type, id, vote):
     url_friendly_content_type = content_type
     content_type = content_type.replace("-", ".")
     if request.is_ajax():
-        return views.vote(request, content_type=content_type, object_id=id, vote=vote, template_name='likes/inclusion_tags/likes.html', can_vote_test=can_vote, extra_context={'can_vote': False, "content_type": url_friendly_content_type})
+        return views.vote(request, content_type=content_type, object_id=id, vote=vote, template_name='likes/inclusion_tags/likes.html', can_vote_test=can_vote, extra_context={'can_vote': False, 'vote_status': 'voted', "content_type": url_friendly_content_type})
     else:
         redirect_url = request.META['HTTP_REFERER']
         return views.vote(request, content_type=content_type, object_id=id, vote=vote, redirect_url=redirect_url, can_vote_test=can_vote)
