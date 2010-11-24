e0c63a13772f41d8ecab49383f83b4f595d87e1e
shaunsephton <shaun@praekeltconsulting.com>
2010-11-24 10:48:40 +0200

    better long desc. version bump

--- setup.py
+++ setup.py
@@ -2,9 +2,9 @@ from setuptools import setup, find_packages
 
 setup(
     name='django-likes',
-    version='0.0.1',
+    version='0.0.2',
     description='Django app providing view interface to django-secretballot.',
-    long_description = open('README.rst', 'r').read(),
+    long_description = open('README.rst', 'r').read() + open('AUTHORS.rst', 'r').read() + open('CHANGELOG.rst', 'r').read(),
     author='Praekelt Foundation',
     author_email='dev@praekelt.com',
     license='BSD',
