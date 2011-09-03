2a2e81f8574786b61eb716af36e91e53c158a191
hedleyroos <hedleyroos@gmail.com>
2011-09-03 11:25:26 +0200

    Avoid unnecessary signal if secretballot is not enabled for the class itself

--- likes/utils.py
+++ likes/utils.py
@@ -1,7 +1,19 @@
 from likes.signals import likes_enabled_test, can_vote_test
 from likes.exceptions import LikesNotEnabledException, CannotVoteException
 
+def _votes_enabled(obj):
+    """See if voting is enabled on the class. Made complicated because
+    secretballot.enable_voting_on takes parameters to set attribute names, so
+    we can't safely check for eg. "add_vote" presence on obj. The safest bet is
+    to check for the 'votes' attribute. 
+    
+    The correct approach is to contact the secretballot developers and ask them
+    to set some unique marker on a class that can be voted on."""
+    return hasattr(obj.__class__, 'votes')
+
 def likes_enabled(obj, request):
+    if not _votes_enabled(obj):
+        return False
     try:
         likes_enabled_test.send(obj, request=request)
     except LikesNotEnabledException:
@@ -9,10 +21,8 @@ def likes_enabled(obj, request):
     return True
 
 def can_vote(obj, user, request):
-    # todo: need a check here to see if voting is enabled on the class. Made
-    # complicated because secretballot.enable_voting_on takes parameters to set
-    # method names, so we can't safely check for eg. "add_vote" method presence
-    # on obj.
+    if not _votes_enabled(obj):
+        return False
     try:
         can_vote_test.send(obj, user=user, request=request)
     except CannotVoteException as e:
