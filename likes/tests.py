207d9f96765d4f1349e881b902f35a28ed85c767
hedleyroos <hedleyroos@gmail.com>
2011-09-06 11:25:53 +0200

    Moved settings file to module

--- likes/tests.py
+++ likes/tests.py
@@ -6,4 +6,5 @@ from likes.templatetags import likes_inclusion_tags
 
 class TestCase(unittest.TestCase):
     def test_something(self):
+        1 == 2
         raise NotImplementedError('Test not implemented. Bad developer!')
