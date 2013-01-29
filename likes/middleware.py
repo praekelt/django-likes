8f6dafa5b3e843c39b9f66aef56becbd600cb520
Rizmari Versfeld <rizziepit@gmail.com>
2013-01-29 09:47:35 +0200

    first attempt at handling no user agent gracefully

--- likes/middleware.py
+++ likes/middleware.py
@@ -1,7 +1,15 @@
+from django.http import HttpResponseBadRequest
+
 from secretballot.middleware import SecretBallotIpUseragentMiddleware
 
 
 class SecretBallotUserIpUseragentMiddleware(SecretBallotIpUseragentMiddleware):
+    def process_request(self, request):
+        if 'REMOTE_ADDR' in request.META and 'HTTP_USER_AGENT' in request.META:
+            super(SecretBallotIpUseragentMiddleware, self).process_request(request)
+        else:
+            return HttpResponseBadRequest()
+
     def generate_token(self, request):
         if request.user.is_authenticated():
             return request.user.username
