985a4804d7646fc445f523eda16523ba82bdc51a
Charles Hathaway <chathaway@logrit.com>
2013-08-06 15:32:06 -0400

    Needed to add in a $(document).ready delay

--- likes/static/likes/includes/likes.js
+++ likes/static/likes/includes/likes.js
@@ -1,17 +1,20 @@
 if (typeof $ != 'undefined')
 {
 
-$('div.liker').delegate('a', 'click', function(event){
-    event.preventDefault();
-    var el = $(this);
-    var replace_selector = el.attr('replace_selector');
-    if (!replace_selector)
-        var replace_target = el.parents('.likes:first');
-    else
-        var replace_target = $(replace_selector);
-    $.get(el.attr('href'), {}, function(data){
-        replace_target.html(data); 
+$(document).ready(function() {
+    $('div.liker').delegate('a', 'click', function(event){
+        event.preventDefault();
+        var el = $(this);
+        var replace_selector = el.attr('replace_selector');
+        if (!replace_selector)
+            var replace_target = el.parents('.likes:first');
+        else
+            var replace_target = $(replace_selector);
+        $.get(el.attr('href'), {}, function(data){
+            replace_target.html(data); 
+        });
     });
+
 });
 
 }
